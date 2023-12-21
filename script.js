2bf9891e558ffcfd43a4d8edc7a05315c33408e1
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-21 20:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+function removeselectColor() {
+  var dropdown = document.getElementById("colorSelect");
+  
+  if (dropdown.selectedIndex !== -1) {
+    dropdown.remove(dropdown.selectedIndex);
+  }
+}
